c36b4b474e15cf3f4eb1cb4eb773a6cd7241dff5
GrigorySmirnovSpb <smirnov2006@mail.ru>
2026-08-12 19:05:05 +0300

    Add slowing on grass and edit obsidian

--- engine/scripts/player.gd
+++ engine/scripts/player.gd
@@ -1,43 +1,49 @@
 extends CharacterBody2D
 
 const SPEED = 3500
+const speed_mult: float = 1.0
 
 @onready var anim = $AnimPlayer
 @onready var tilemap = $"../TileMapLayer/map/osnova"
 var direction: Vector2 = Vector2(0, 0)
-var speed_mult: float = 1.0
+
+func _ready() -> void:
+	print("TileMap position: ", tilemap.position)
+	print("TileMap global position: ", tilemap.global_position)
 
 func _physics_process(delta: float) -> void:
 	# Передвижение персонажа
 	direction = Vector2(Input.get_axis("left", "right"), Input.get_axis("up", "down")).normalized()
-	get_speed_multiplier()
+	var speed_multiplier = get_speed_multiplier()
 	if is_move():
-		move(delta)
+		move(speed_multiplier, delta)
 	else:
 		idle(delta)
 	move_and_slide()
-	speed_mult = 1.0
 	# Сохранение позиции игрока
 	Global.player_position = global_position
 
-func get_speed_multiplier() -> void:
-	var position = tilemap.local_to_map(global_position)
-	var tile_data = tilemap.get_cell_tile_data(position)
+func get_speed_multiplier() -> float:
+	var local_pos = tilemap.to_local(global_position + Vector2(0, 10))
+	var tile_pos = tilemap.local_to_map(local_pos)
+	var tile_data = tilemap.get_cell_tile_data(tile_pos)
 	
-	if tile_data and tile_data.get_custom_data("speed_multiplier"):
-		print("Yes")
-		speed_mult = speed_mult - tile_data.get_custom_data("speed_multiplier")
-		print(speed_mult)
-	#print(speed_mult)
+	if tile_data:
+		#print("Yes")
+		print(tile_data.get_custom_data("speed_multiplier"))
+		return speed_mult - tile_data.get_custom_data("speed_multiplier")
+	else:
+		return speed_mult
+		
 func is_move() -> bool:
 	if direction != Vector2(0, 0):
 		return true
 	else: 
 		return false
 
-func move(delta: float):
+func move(speed_multiplier: float, delta: float):
 	anim_move()
-	velocity = SPEED * direction * speed_mult * delta
+	velocity = SPEED * direction * speed_multiplier * delta
 
 func anim_move():
 	if direction == Vector2(0, 1):
